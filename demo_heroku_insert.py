0bbb66f7e9f8277fdcd25f279398407d97ec618c
Pratyush Debnath <pratyush.debnath93@gmail.com>
2020-01-20 12:40:14 +0530

    Update demo_heroku_insert.py

--- demo_heroku_insert.py
+++ demo_heroku_insert.py
@@ -6,7 +6,7 @@ from simple_salesforce import Salesforce
 @sched.scheduled_job('cron', day_of_week='mon', hour=13)
 def scheduled_job():
 	print('Connecting to the Salesforce Org')
-	sf = Salesforce(username='pratyush.debnath@gehc.amer.pdxpocdev',password='Pratyush@96',security_token='ntFDdxOuQ1jNCqAsKxlKYIhwl',domain='test')
+	sf = Salesforce(username='enter your username',password='enter your password',security_token='enter security token',domain='test')
 	sf.ELTON__Equipment__c.create({'GEHC_Inventory_Org__c':'AUS','ELTONGEHC__Inventory_Org__c':'AUS'})
 	print('The job is completed by creating record in demo inventory')
 
